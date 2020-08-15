b1d0f0b1f06dc2788f2e56f55d08fc1af19d3dda
NycollSM <nycollsotomorales21@gmail.com>
2020-08-15 02:15:01 -0700

    adding design section without responsive styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/headercont/header';
+import Project from './components/projects/projects';
+import Design from './components/design/design';
 import './sass/styles.scss';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Project></Project>
+      <Design></Design>
     </div>
   );
 }
