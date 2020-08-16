a5d3375b0e6bb6d3c55d27d48c9edf12096dcd14
NycollSM <nycollsotomorales21@gmail.com>
2020-08-16 01:12:37 -0700

    Updates

---
+++ precache-manifest.95e9f064be9a30bb57d5de210d604a38.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025280c21968c3d595a28bcefa71271e",
+    "url": "/nycollsmoralesportfolio/index.html"
+  },
+  {
+    "revision": "16836592d9d823ddd7d4",
+    "url": "/nycollsmoralesportfolio/static/css/main.b9ab90ff.chunk.css"
+  },
+  {
+    "revision": "074c1a449fc75c90789a",
+    "url": "/nycollsmoralesportfolio/static/js/2.45187d2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nycollsmoralesportfolio/static/js/2.45187d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16836592d9d823ddd7d4",
+    "url": "/nycollsmoralesportfolio/static/js/main.d95db817.chunk.js"
+  },
+  {
+    "revision": "848dd0ec0a285dd8aed7",
+    "url": "/nycollsmoralesportfolio/static/js/runtime-main.950af09e.js"
+  },
+  {
+    "revision": "0b824ca649aba0f055a5d97ac7acd12d",
+    "url": "/nycollsmoralesportfolio/static/media/E-commerce.0b824ca6.jpg"
+  },
+  {
+    "revision": "333183115b4663eaf0a0a0b775749f72",
+    "url": "/nycollsmoralesportfolio/static/media/Ecommerce.33318311.png"
+  },
+  {
+    "revision": "4ce824d31a2c10fe0c05687773fa69e2",
+    "url": "/nycollsmoralesportfolio/static/media/camaleon.4ce824d3.jpg"
+  },
+  {
+    "revision": "b35da379f7d1b6d67e564cfaef410f7f",
+    "url": "/nycollsmoralesportfolio/static/media/cherry.b35da379.png"
+  },
+  {
+    "revision": "4f7c7e586983d7188c6f2dbeedcce3c1",
+    "url": "/nycollsmoralesportfolio/static/media/dogegram-mockup.4f7c7e58.jpg"
+  },
+  {
+    "revision": "da762be4bcb3853a3b734ede93ca87be",
+    "url": "/nycollsmoralesportfolio/static/media/mockup.da762be4.jpg"
+  },
+  {
+    "revision": "a3a8e188c21e936bf7c014103c4bfd3e",
+    "url": "/nycollsmoralesportfolio/static/media/municipalidad.a3a8e188.jpg"
+  },
+  {
+    "revision": "75a79ca3a40acc34f4a1588c4c16526d",
+    "url": "/nycollsmoralesportfolio/static/media/proxy2.75a79ca3.PNG"
+  },
+  {
+    "revision": "bed8754cbcec8ac6556e9ba1aabee330",
+    "url": "/nycollsmoralesportfolio/static/media/trivias.bed8754c.PNG"
+  }
+]);
